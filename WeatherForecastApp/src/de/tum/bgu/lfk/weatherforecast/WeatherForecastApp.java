cfb1fa7ffa128947ddff7f95c8ff481f47028507
Mathias Jahnke <mjahnke@outlook.com>
2015-05-26 16:42:11 +0200

    further

--- WeatherForecastApp/src/de/tum/bgu/lfk/weatherforecast/WeatherForecastApp.java
+++ WeatherForecastApp/src/de/tum/bgu/lfk/weatherforecast/WeatherForecastApp.java
@@ -3,17 +3,24 @@ package de.tum.bgu.lfk.weatherforecast;
 import org.gicentre.geomap.GeoMap;
 
 import processing.core.PApplet;
+import processing.core.PVector;
+import processing.data.*;
 
 @SuppressWarnings("serial")
 public class WeatherForecastApp extends PApplet{
 	
-	private GeoMap geoMap;
+	private GeoMap geoMapCountries;
+	private GeoMap geoMapCities;
+	private JSONObject obj;
 	
 	public void setup(){
 		size(700,700);
 		
-		geoMap = new GeoMap(0, 0, 500, 300, this);
-		geoMap.readFile("C:/Users/Mathias/workspace/data/countries/cntry00");
+		geoMapCountries = new GeoMap(0, 0, 500, 300, this);
+		geoMapCountries.readFile("C:/Users/Mathias/workspace/data/countries/cntry00");
+		
+		geoMapCities = new GeoMap(0, 0, 500, 300, this);
+		geoMapCities.readFile("C:/Users/Mathias/workspace/data/countries/cntry00");
 		
 	}
 	
@@ -23,8 +30,19 @@ public class WeatherForecastApp extends PApplet{
 		
 		fill(210, 210, 210);
 		stroke(235);
-		geoMap.draw();
+		geoMapCountries.draw();
 		
+	}
+	
+	public void mouseClicked(){
+		PVector pv = geoMapCountries.screenToGeo(mouseX, mouseY);
+		println(pv);
+	}
+	
+	private JSONObject getJSONObject(float lat, float lon){
+		JSONObject obj = null;
+		
+		return obj;
 	}
 
 }
