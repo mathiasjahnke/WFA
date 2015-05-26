396f730f5cabc9f9fe819401645192c63704e34a
Mathias Jahnke <mjahnke@outlook.com>
2015-05-26 16:29:12 +0200

    load data

--- WeatherForecastApp/src/de/tum/bgu/lfk/weatherforecast/WeatherForecastApp.java
+++ WeatherForecastApp/src/de/tum/bgu/lfk/weatherforecast/WeatherForecastApp.java
@@ -1,5 +1,30 @@
 package de.tum.bgu.lfk.weatherforecast;
 
-public class WeatherForecastApp {
+import org.gicentre.geomap.GeoMap;
+
+import processing.core.PApplet;
+
+@SuppressWarnings("serial")
+public class WeatherForecastApp extends PApplet{
+	
+	private GeoMap geoMap;
+	
+	public void setup(){
+		size(700,700);
+		
+		geoMap = new GeoMap(0, 0, 500, 300, this);
+		geoMap.readFile("C:/Users/Mathias/workspace/data/countries/cntry00");
+		
+	}
+	
+	
+	public void draw(){
+		background(55);
+		
+		fill(210, 210, 210);
+		stroke(235);
+		geoMap.draw();
+		
+	}
 
 }
